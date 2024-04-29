b555d377296525333c1cbb7bda3c68d5f39ff925
GEOkettle <jsonmiou@gmail.com>
2024-04-29 22:56:20 +0900

    dsf

--- server/server.js
+++ server/server.js
@@ -30,7 +30,7 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 if (process.env.NODE_ENV === 'prd') { 
-  const whitelist = [`${process.env.PRD_ADDR}`, `${process.env.PRD_ADDR2}`];
+  const whitelist = [`${process.env.PRD_ADDR}`, `${process.env.PRD_ADDR2}`,'172.0.0.1','192.168.0.38','172.0.1.1'];
 
 }
 const corsOptions = {
